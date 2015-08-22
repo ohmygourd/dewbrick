8d12a8da25fdee71dac870d5130187a33c0752b6
Wes Mason <wes@1stvamp.org>
2015-08-22 16:00:57 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Installer for dewbrick
+"""
+
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='debrick',
+    description='',
+    long_description=open('README.rst').read(),
+    version='1.0.0',
+    author='Team Oh My Gourd',
+    author_email='debrick@example.com',
+    url='https://github.com/ohmygourd/debrick',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=open('requirements.txt').readlines(),
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    license='Apache'
+)
