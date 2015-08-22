f4b3cf2df8654e7b3c6a66458db95410ef6933f1
antony <antony@lister>
2015-08-22 16:45:37 +0100

    Sample usage of turn data structure.

--- app.py
+++ app.py
@@ -5,8 +5,27 @@ from tornado import template
 
 
 class MainHandler(tornado.web.RequestHandler):
+
+    DEMO_TURN = {
+        'player_id': 'abc',
+        'player_turn': 1,
+        'card': {
+            'id': 'card_1',
+            'name': 'Card Name',
+            'image': None,
+            'description': 'This is a card',
+            'attributes': {
+                'power': 9001,
+                'strength': 100,
+                'speed': 50,
+                'agility': 20,
+                'smell': 4
+            }
+        }
+    }
+
     def get(self):
-        self.write(application.template_loader.load("index.html").generate(myvalue="XXX"))
+        self.write(application.template_loader.load("index.html").generate(turn=self.DEMO_TURN))
 
 
 class SocketHandler(WebSocketHandler):
