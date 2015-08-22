b0b1df363b147e3845e4db232d576671e6cabd40
Wes Mason <wes@1stvamp.org>
2015-08-22 21:07:56 +0100

    Convert API fetching to use Tornado async

--- dewbrick/majesticapi.py
+++ dewbrick/majesticapi.py
@@ -2,11 +2,14 @@ import argparse
 import json
 import os
 
-import requests
 from itertools import cycle
+
+from tornado import gen, httpclient, ioloop
+from tornado.httputil import url_concat
 
 BASE_URL = "https://api.majestic.com/api/json"
 BASE_PARAMS = {'app_api_key': os.environ.get('MAJESTIC_API_KEY')}
+CLIENT = httpclient.AsyncHTTPClient()
 
 
 class GameDataSet(object):
@@ -39,53 +42,65 @@ class GameDataSet(object):
         self._data.remove(item)
 
 
-def get(cmd, params):
-    querydict = {'cmd': cmd}
-    querydict.update(BASE_PARAMS)
-    querydict.update(params)
-    response = requests.get(BASE_URL, params=querydict)
-    return json.loads(response.text)
+@gen.coroutine
+def majestic_get(cmd, params):
+    query = {'cmd': cmd}
+    query.update(BASE_PARAMS)
+    query.update(params)
+    request = httpclient.HTTPRequest(url_concat(BASE_URL, query),
+                                     method="GET")
+    response = yield CLIENT.fetch(request)
+    raise gen.Return(json.loads(response.body.decode('utf-8')))
 
 
-def getIndexItemInfo(sitelist):
-
+@gen.coroutine
+def get_card_stats(cards):
     cmd = 'GetIndexItemInfo'
-    params = {'items': len(sitelist),
+    params = {'items': len(cards),
               'datasource': 'fresh'}
-    items = {'item{0}'.format(i): site for i, site in enumerate(sitelist)}
+    items = {'item{0}'.format(i): card for i, card in enumerate(cards)}
     params.update(items)
 
-    responsedata = get(cmd, params)
+    responsedata = yield majestic_get(cmd, params)
     if responsedata['Code'] == 'OK':
+        results = []
         for data in responsedata['DataTables']['Results']['Data']:
-            yield {
+            results.append({
                 'speed': data['OutDomainsExternal'] + 1,
                 'power': data['OutLinksExternal'] + 1,
                 'agility': data['OutLinksInternal'] + 1,
                 'strength': data['RefDomainsEDU'] + 1,
                 'smell': data['CitationFlow'] + 1,
-            }
+            })
+        raise gen.Return(results)
     else:
-        yield {}
+        raise gen.Return({})
 
-def searchByKeyword(key, start=0, count=10):
 
+@gen.coroutine
+def find_cards(key, start=0, count=10):
     cmd = 'SearchByKeyword'
     params = {'query': key, 'scope': '1', 'from': start, 'count': count}
-    responsedata = get(cmd, params)
+    responsedata = yield majestic_get(cmd, params)
+    results = []
     if responsedata['Code'] == 'OK':
         for data in responsedata['DataTables']['Results']['Data']:
-            yield data['Item']
+            results.append(data['Item'])
+    raise gen.Return(results)
 
+
+@gen.coroutine
 def run():
     parser = argparse.ArgumentParser(description="a test thing")
     parser.add_argument('urls', nargs='+')
 
     args = parser.parse_args()
-    results = getIndexItemInfo(args.urls)
+    results = yield get_card_stats(args.urls)
     for result in results:
         print(result)
 
+    cards = yield find_cards('python', start=10, count=10)
+    print(list(d for d in cards))
+
 if __name__ == '__main__':
-    run()
-    print(list(d for d in searchByKeyword('python', start=10, count=10)))
+    ioloop.IOLoop.current().run_sync(run)
