f9015c90b9b48183fceedbc5df70e03cf3e36663
Antony Semonella <asemonella@gmail.com>
2015-08-22 19:25:20 +0100

    Towards tracking game state.

--- dewbrick/app.py
+++ dewbrick/app.py
@@ -35,8 +35,80 @@ class MainHandler(tornado.web.RequestHandler):
 
 class SocketHandler(WebSocketHandler):
 
-    wesisright = {
-        "turn": "1"
+    # TODO: Populate card lists from API backend
+
+    cards_p1 = [
+        {
+            'id': 'card_1',
+            'name': 'Card Name',
+            'image': None,
+            'description': 'This is a card',
+            'attributes': [
+                {'name': 'power', 'value': 9001},
+                {'name': 'strength', 'value': 100},
+                {'name': 'speed', 'value': 50},
+                {'name': 'agility', 'value': 20},
+                {'name': 'smell', 'value': 4}
+            ]
+        },
+        {
+            'id': 'card_2',
+            'name': 'Card Name 2',
+            'image': None,
+            'description': 'This is card 2',
+            'attributes': [
+                {'name': 'power', 'value': 90},
+                {'name': 'strength', 'value': 10},
+                {'name': 'speed', 'value': 80},
+                {'name': 'agility', 'value': 30},
+                {'name': 'smell', 'value': 2}
+            ]
+        }
+    ]
+
+    cards_p2 = [
+        {
+            'id': 'card_3',
+            'name': 'Card Name 3',
+            'image': None,
+            'description': 'This is card 3',
+            'attributes': [
+                {'name': 'power', 'value': 1},
+                {'name': 'strength', 'value': 180},
+                {'name': 'speed', 'value': 90},
+                {'name': 'agility', 'value': 30},
+                {'name': 'smell', 'value': 9}
+            ]
+        },
+        {
+            'id': 'card_4',
+            'name': 'Card Name 4',
+            'image': None,
+            'description': 'This is card 4',
+            'attributes': [
+                {'name': 'power', 'value': 980},
+                {'name': 'strength', 'value': 110},
+                {'name': 'speed', 'value': 890},
+                {'name': 'agility', 'value': 10},
+                {'name': 'smell', 'value': 222}
+            ]
+        }
+    ]
+
+    players = [
+        {
+            'player_id': 'abc',
+            'player_name': 'Dave Lister',
+        },
+        {
+            'player_id': 'cba',
+            'player_name': 'Arnold Rimmer',
+        }
+    ]
+
+    game_state = {
+        'turn_no': 0,
+        'player_turn': None,
     }
 
     handlers = []
@@ -49,9 +121,7 @@ class SocketHandler(WebSocketHandler):
         print("WebSocket opened")
 
     def on_message(self, message):
-        self.wesisright['turn'] = "2"
         self.write_message(json.dumps(DEMO_TURN))
-        #self.write(json.dumps(DEMO_TURN))
 
     def on_close(self):
         print("WebSocket closed")
