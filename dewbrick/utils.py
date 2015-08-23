7a2a0ca5cf57908ec8968c6ce9e624b10fba6cf8
Wes Mason <wes@1stvamp.org>
2015-08-23 01:46:38 +0100

    Add screenshot caching

--- dewbrick/utils.py
+++ dewbrick/utils.py
@@ -1,3 +1,4 @@
+import os
 import hashlib
 import json
 import tldextract
@@ -6,12 +7,28 @@ from random import choice
 import requests
 from urllib.parse import quote
 
+IMAGE_CACHE_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                                'cache/images.json')
+
+
+def get_image_cache():
+    if os.path.exists(IMAGE_CACHE_PATH):
+        with open(IMAGE_CACHE_PATH) as f:
+            return json.load(f)
+    return {}
+
+
+def save_image_cache():
+    with open(IMAGE_CACHE_PATH, 'w+') as f:
+        f.write(json.dumps(IMAGE_CACHE))
+
 
 GOOGLE_URL = 'https://www.googleapis.com/pagespeedonline/v1/runPagespeed'
 ROBOHASH_URL = 'http://robohash.org/'
 TITLES = ('Mister', 'Little Miss', 'Señor', 'Queen')
 SUFFIXES = ('Destroyer of Worlds', 'the Monkey Botherer', 'PhD',
             'Ah-gowan-gowan-gowan')
+IMAGE_CACHE = get_image_cache()
 
 
 def generate_name(domain):
@@ -48,11 +65,17 @@ def generate_image(name):
 def generate_screenshot(name):
     if 'http' not in name:
         name = 'http://{}'.format(name)
+    if name not in IMAGE_CACHE:
+        url = ('{}?url={}&screenshot=true').format(GOOGLE_URL, quote(name, ''))
+        response = requests.get(url)
+        data = json.loads(response.text)
+        if 'screenshot' in data:
+            img_data = data['screenshot']['data']
+            img_data = img_data.replace('_', '/').replace('-', '+')
+            img_data = 'data:image/gif;base64,{}'.format(img_data)
 
-    url = ('{}?url={}&screenshot=true').format(GOOGLE_URL, quote(name, ''))
-    response = requests.get(url)
-    data = json.loads(response.text)
-    if 'screenshot' in data:
-        img_data = data['screenshot']['data']
-        img_data = img_data.replace('_', '/').replace('-', '+')
-        return 'data:image/gif;base64,{}'.format(img_data)
+            IMAGE_CACHE[name] = img_data
+        else:
+            IMAGE_CACHE[name] = False
+        save_image_cache()
+    return IMAGE_CACHE[name]
